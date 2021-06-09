1e4297ddcfc6479b3dc924d369d9c6b08f0bfa21
WK-15\Shark Lin <lin22223344@gmail.com>
2021-06-09 10:49:45 +0800

    第一版API範例

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+
+	public class ProductController : ApiController
+	{
+
+		ExampleEntities exampleEntities = new ExampleEntities();
+
+		[Route("product")]
+		[HttpGet]
+		public IEnumerable<PRODUCT> Get()
+		{
+			return exampleEntities.PRODUCT;
+		}
+
+		[Route("product/{id}")]
+		[HttpGet]
+		public PRODUCT Get(int id)
+		{
+			return exampleEntities.PRODUCT.Find(id);
+		}
+
+		/// <summary>
+		/// 搜尋產品
+		/// </summary>
+		/// <param name="pageNumber">頁碼</param>
+		/// <param name="pageSize">大小</param>
+		/// <returns>產品列表</returns>
+		[Route("product/search")]
+		[HttpGet]
+		public IEnumerable<PRODUCT> Get(int pageNumber, int pageSize)
+		{
+			return exampleEntities.PRODUCT.Skip(pageNumber * pageSize).Take(pageSize);
+		}
+
+		[Route("product")]
+		[HttpPost]
+		public void Post([FromBody] PRODUCT product)
+		{
+			exampleEntities.PRODUCT.Add(product);
+			exampleEntities.SaveChanges();
+		}
+
+		[Route("product")]
+		[HttpPut]
+		public void Put([FromBody] PRODUCT product)
+		{
+			exampleEntities.PRODUCT.Find(product.ID).NAME = product.NAME;
+			exampleEntities.SaveChanges();
+		}
+
+		[Route("product/{id}")]
+		[HttpDelete]
+		public void Delete(int id)
+		{
+			PRODUCT product = exampleEntities.PRODUCT.Find(id);
+			exampleEntities.PRODUCT.Remove(product);
+			exampleEntities.SaveChanges();
+		}
+
+		[Route("test")]
+		[HttpGet]
+		public string Test()
+		{
+			return Resources.LanguageResource.test;
+		}
+	}
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using NSwag.AspNet.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+            var config = new HttpConfiguration();
+            app.UseSwaggerUi3(typeof(Startup).Assembly, settings =>
+            {
+                
+                settings.GeneratorSettings.DefaultUrlTemplate = "{controller}/{action}/{id?}";
+                settings.PostProcess = document =>
+                {
+                    document.Info.Title = "WebAPI 範例";
+                };
+            });
+            app.UseWebApi(config);
+            config.MapHttpAttributeRoutes();
+            config.EnsureInitialized();
+        }
+	}
+}
